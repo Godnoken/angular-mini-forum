0d5ef2a236d624378580a4bcdf780ef5a3bb992c
Sebastian Ojefors <godnoken@gmail.com>
2022-03-28 18:21:29 +0200

    Add reset.scss and simple app layout for desktop

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForumComponent } from './components/forum/forum.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: ForumComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
